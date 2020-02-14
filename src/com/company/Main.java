aef97be69bcd0c956a0e66fa0855613720a72daa
swapnil29ahirrao <sahirrao@xpanxion.com>
2020-02-14 13:25:13 +0530

    First commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Project");
-
+        System.out.println("Project test");
+        System.out.println("test");
     }
 }
 
