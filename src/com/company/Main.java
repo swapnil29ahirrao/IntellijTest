9579f36e55d653f75a0ad4d4793c7523d36c93ab
swapnil29ahirrao <44319301+swapnil29ahirrao@users.noreply.github.com>
2020-02-14 14:49:17 +0530

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Project test");
         System.out.println("test");
+        System.out.println("work");
     }
 }
 
